241d63b8caf211aaa631d8a14a7cea6e63ea8f42
Josh Sargento <josh-sargento@Joshs-MacBook-Air.local>
2023-08-09 03:09:07 +0800

    test3333333433

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const app = express();
 app.use("/get", titleRoute);
 app.use("/transcribe", transcribeRoute);
 app.use("/mp4", transcribeMp4Route);
-app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
     path.join(__dirname, "./client/build/index.html"),
