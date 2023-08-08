daf05986431a3f523b440a2f5f3c58610587c9e2
Josh Sargento <josh-sargento@Joshs-MacBook-Air.local>
2023-08-08 19:12:03 +0800

    fixing server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import titleRoute from "./routes/title.js";
-import transcribeRoute from "./routes/transcribe.js";
-import transcribeMp4Route from "./routes/transcribemp4.js";
+import titleRoute from "./server/routes/title.js";
+import transcribeRoute from "./server/routes/transcribe.js";
+import transcribeMp4Route from "./server/routes/transcribemp4.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use("/get", titleRoute);
 app.use("/transcribe", transcribeRoute);
 app.use("/mp4", transcribeMp4Route);
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
+    path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
