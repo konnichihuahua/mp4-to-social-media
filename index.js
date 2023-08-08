ffb190a269a344c2cd7a76e61dc75fc434fd43b5
Josh Sargento <josh-sargento@Joshs-MacBook-Air.local>
2023-08-08 19:26:48 +0800

    fixingngngng

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ import titleRoute from "./server/routes/title.js";
 import transcribeRoute from "./server/routes/transcribe.js";
 import transcribeMp4Route from "./server/routes/transcribemp4.js";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json());
