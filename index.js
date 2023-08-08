fd3c8643d884ff01619ec8e6b593ff35e1a00d14
Josh Sargento <josh-sargento@Joshs-MacBook-Air.local>
2023-08-08 20:17:50 +0800

    inceased json size

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use("/get", titleRoute);
 app.use("/transcribe", transcribeRoute);
 app.use("/mp4", transcribeMp4Route);
